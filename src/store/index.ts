783e4c01bd0097a804bce82865dc180525d9d552
kotori yang <yzjkid9@gmail.com>
2021-12-03 22:45:44 +0800

    Added selected websites listening

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,17 +2,29 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: {
-    inputActiveStatus: [false, true] as [boolean, boolean]
+    inputActiveStatus: [true, false] as [boolean, boolean],
+    selectedSites: [] as Array<string>,
+    tasks: [] as Array<Task>
   },
   mutations: {
     EXCHANGE_INPUT_STATUS (state) {
       const tmp = state.inputActiveStatus
       state.inputActiveStatus = tmp.map(elem => !elem) as [boolean, boolean]
+    },
+    UPDATE_SELECTED_WEBSITES (state, params: {site: string, select: boolean}) {
+      if (params.select) {
+        state.selectedSites.push(params.site)
+        return
+      }
+      state.selectedSites = state.selectedSites.filter(site => site !== params.site)
     }
   },
   actions: {
     exchange_input_status (context) {
       context.commit('EXCHANGE_INPUT_STATUS')
+    },
+    update_selected_websites (context, params: {site: string, select: boolean}) {
+      context.commit('UPDATE_SELECTED_WEBSITES', params)
     }
   },
   modules: {
--- src/views/Home/HomeSelect/select.ts
+++ src/views/Home/HomeSelect/select.ts
@@ -1,11 +1,21 @@
+import store from '@/store'
+
 export function select ($event: Event): void {
   const target = ($event.target as HTMLElement).parentElement as HTMLElement
   const text = target.getElementsByTagName('span')[0] as HTMLElement
   if (target.classList.contains('active')) {
     target.classList.remove('active')
     text.classList.remove('active')
+    store.dispatch('update_selected_websites', {
+      site: target.innerText,
+      select: false
+    }).then()
     return
   }
   target.classList.add('active')
   text.classList.add('active')
+  store.dispatch('update_selected_websites', {
+    site: target.innerText,
+    select: true
+  }).then()
 }
