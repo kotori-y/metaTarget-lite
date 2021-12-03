dd085c637b518bd19eb1331f9d205a019b71117b
kotori yang <yzjkid9@gmail.com>
2021-12-03 11:10:17 +0800

    Completed the basic file selecting style

--- src/views/Home/HomeInput/HomeInputFile.vue
+++ src/views/Home/HomeInput/HomeInputFile.vue
@@ -1,5 +1,10 @@
 <template>
   <div :class="{'input-mol': true, active: nowStatus}">
+    <div class="file-container" v-show="nowStatus" >
+      <input type="file" class="custom-file-input" id="file" accept=".text/.csv">
+      <span>Upload or drop your .xml file</span>
+    </div>
+    <HomeInputButton v-show="nowStatus"></HomeInputButton>
     <Mask v-show="!nowStatus" @click="adjustedWidth"></Mask>
   </div>
 </template>
@@ -9,10 +14,11 @@ import './style.css'
 import store from '@/store'
 import { defineComponent, computed } from 'vue'
 import Mask from '@/components/Mask.vue'
+import HomeInputButton from '@/views/Home/HomeInput/HomeInputButton.vue'
 
 export default defineComponent({
   name: 'HomeInputFile',
-  components: { Mask },
+  components: { HomeInputButton, Mask },
   setup () {
     // Adjusted width
     const nowStatus = computed(() => store.state.inputActiveStatus[1])
@@ -30,18 +36,65 @@ export default defineComponent({
 
 <style scoped>
 
-.input-mol label {
-  font-size: 24px;
-  font-weight: 500;
+.file-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
 }
 
-#single-smiles {
-  width: 90%;
-  border: none;
-  background: rgba(250, 235, 215, 0.7);
-  font-size: 20px;
+.input-mol span {
+  position: relative;
+  font-size: 18px;
+  font-weight: 500;
+  top: -3vh;
+}
+
+.custom-file-input::-webkit-file-upload-button {
+  visibility: hidden;
+}
+
+.custom-file-input {
+  visibility: hidden;
+  position: relative;
+  top: 3vh;
+  width: 50vw;
+}
+
+.custom-file-input::before {
+  content: "";
+  background-image: url("../../../assets/file-unselected.svg");
+  display: inline-block;
+  border: 2px dashed #999;
+  border-radius: 3px;
+  padding: 5px 8px;
   outline: none;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  white-space: nowrap;
+  -webkit-user-select: none;
+  cursor: pointer;
+  /*text-shadow: 1px 1px #fff;*/
+  font-weight: 700;
+  font-size: 10pt;
+  visibility: initial;
+  color: bisque;
+  height: 15vh;
+  width: 50vw;
+  background-size: 7%;
+  background-repeat: no-repeat;
+  background-position: 50% 23%;
+}
+
+.custom-file-input.active::before {
+  background-image: url("../../../assets/file-selected.svg");
+}
+
+.custom-file-input:hover::before {
+  border-color: black;
+}
+
+.custom-file-input:active::before {
+  background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9);
 }
 
 </style>
